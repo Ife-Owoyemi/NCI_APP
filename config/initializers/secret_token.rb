5f9feb36cc907e51203338a33f809e364d577834
Ife-Owoyemi <cyberife7@gmail.com>
2013-12-03 02:19:33 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NciApp::Application.config.secret_token = 'f5f8fd3c7401134d08dad30ad1af87444ed305ca220eac9e0b9e02b7591eaa26a6f71d24dcb308d1925587db22d0fa80e547f53347b595ca6b810a2eff17ace8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NciApp::Application.config.session_store :cookie_store, key: '_nciApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NciApp::Application.config.session_store :active_record_store
